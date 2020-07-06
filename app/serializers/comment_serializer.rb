997ef69e5901f9d9e9118be427d8536724fce8c4
joannaylin <lin.joanna.y@gmail.com>
2020-07-05 23:35:07 -0500

    modified comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :note, :user_id, :episode, :start_timestamp
+  attributes :id, :note, :user_id, :episode, :start_timestamp, :created_at, :updated_at
 
   belongs_to :episode
   belongs_to :user
