1006f813cb22cce092b602aa1cd17a593c685d21
joannaylin <lin.joanna.y@gmail.com>
2020-08-27 11:57:44 -0500

    remove access token in json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -42,7 +42,7 @@ class Api::V1::UsersController < ApplicationController
     payload = {user_id: @user.id}
     token = issue_token(payload)
 
-    render json: {token: token, user: { username: @user.username, spotify_url: @user.spotify_url, profile_img_url: @user.profile_img_url, access: auth_params["access_token"] }}
+    render json: {token: token, user: { username: @user.username, spotify_url: @user.spotify_url, profile_img_url: @user.profile_img_url }}
 
   end
 
